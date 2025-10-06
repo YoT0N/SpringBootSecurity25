cac16709dfe9e795c2ddb19288080685d161d953
YoT0N <145184198+YoT0N@users.noreply.github.com>
2025-10-06 20:10:27 +0300

    Add files via upload

---
+++ src/main/java/edu/ilkiv/security25/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package edu.ilkiv.security25.config;
+
+/*
+@author   Bodya
+@project   security25
+@class  SecurityConfig
+@version  1.0.0
+@since 06.10.2025 - 19.03
+*/
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests( auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
